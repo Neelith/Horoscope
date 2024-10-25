21038b11f7f5670dcd8a8286c8cf95e9a439104f
Alessandro Averna <alessandro.averna.1994@gmail.com>
2024-10-25 08:39:14 +0200

    added basic configurations

--- functions/helloworld.ts
+++ functions/helloworld.ts
@@ -1,3 +1,9 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
-  }
+interface Env {
+  TEST_ENV: string;
+}
+
+type Context = EventContext<Env, string, unknown>;
+
+export async function onRequestGet(context: Context): Promise<Response> {
+  return new Response(`req: ${context}`);
+}
