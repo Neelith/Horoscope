c0e5db660986e1561b1aca96612bfefc4fbcc008
Alessandro Averna <alessandro.averna.1994@gmail.com>
2024-10-25 20:20:03 +0200

    worker hello world

--- functions/helloworld.ts
+++
@@ -1,9 +0,0 @@
-interface Env {
-  TEST_ENV: string;
-}
-
-type Context = EventContext<Env, string, unknown>;
-
-export async function onRequestGet(context: Context): Promise<Response> {
-  return Response.json(context);
-}
