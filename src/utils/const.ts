bf32582a33ce89feba490c46e9a8952f366f4bbe
Alessandro Averna <alessandro.averna.1994@gmail.com>
2025-01-02 22:33:34 +0100

    added authentication and authorization

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,7 @@
 export class Const {
 	//Headers
 	static authorizationHeader: string = 'Authorization';
+	static serviceKeyHeaderName: string = 'X-Service-Key';
 
 	//Params
 	static paramSign: string = 'sign';
